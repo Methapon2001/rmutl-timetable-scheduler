dbd2a21f4c43b541086ec2fface62300bdb6483f
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2023-05-08 10:55:36 +0700

    chore(client): format

--- client/src/routes/timetable/+page.svelte
+++ client/src/routes/timetable/+page.svelte
@@ -289,7 +289,7 @@
         {#if data.section.total === 0}
           <div class="p-8 text-center">
             <h1 class="mb-4 text-5xl font-extrabold">No Data</h1>
-            <h2 class="text-secondary text-3xl">
+            <h2 class="text-3xl text-secondary">
               No section created.<br />Must have section data in order for timetable to show.
             </h2>
           </div>
@@ -336,19 +336,19 @@
     </div>
   </div>
   <div>
-    <div class="section-selector bg-light border-l">
+    <div class="section-selector border-l bg-light">
       {#each data.section.data as section}
         {#if section.parent === null}
           <div class="w-full space-y-2 border-b p-4">
             <div class="mb-2 space-y-2 text-sm">
               <div class="flex gap-2">
                 <span
-                  class="bg-primary inline-block flex items-center rounded px-2 py-1 font-semibold text-white"
+                  class="inline-block flex items-center rounded bg-primary px-2 py-1 font-semibold text-white"
                 >
                   {section.subject.code}
                 </span>
                 <span
-                  class="bg-primary inline-block flex items-center rounded px-2 py-1 font-semibold text-white"
+                  class="inline-block flex items-center rounded bg-primary px-2 py-1 font-semibold text-white"
                 >
                   {section.subject.name}
                 </span>
@@ -356,12 +356,12 @@
 
               <div class="flex gap-2">
                 <span
-                  class="bg-primary inline-block flex items-center rounded px-2 py-1 font-semibold text-white"
+                  class="inline-block flex items-center rounded bg-primary px-2 py-1 font-semibold text-white"
                 >
                   SEC {section.no}
                 </span>
                 <span
-                  class="bg-primary inline-block flex items-center rounded px-2 py-1 font-semibold text-white"
+                  class="inline-block flex items-center rounded bg-primary px-2 py-1 font-semibold text-white"
                 >
                   {section.group?.name ?? 'Any'}
                 </span>
@@ -440,7 +440,7 @@
   </div>
   {#if state.section}
     <div
-      class="border-primary bg-light flex flex justify-between gap-2 overflow-hidden rounded border font-semibold shadow"
+      class="flex flex justify-between gap-2 overflow-hidden rounded border border-primary bg-light font-semibold shadow"
     >
       <span class="bg-primary px-3 py-2 font-semibold text-white">Selected</span>
       <span class="truncate px-4 py-2">
@@ -456,7 +456,7 @@
   {/if}
 
   <div class="alloc-control">
-    <div class="bg-primary grid grid-cols-6 rounded font-semibold text-white">
+    <div class="grid grid-cols-6 rounded bg-primary font-semibold text-white">
       <div class="col-span-5 flex items-center px-4 py-2">
         <input
           class="w-full"
