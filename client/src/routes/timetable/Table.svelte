034d5775e5645b582a13b748246534d6ce03e7dc
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2023-05-11 17:15:50 +0700

    fix(client-timetable): overlap visualise same line

--- client/src/routes/timetable/Table.svelte
+++ client/src/routes/timetable/Table.svelte
@@ -95,19 +95,20 @@
     });
 
     for (let i = 0; i < processed.length; i++) {
-      if (processed[i]._overlap == false) continue;
+      if (processed[i]._overlap == false || processed[i]._offset !== -1) continue;
 
       const offsetList: number[] = [];
 
       const mutualOverlap = processed.filter(
         (item) =>
+          processed[i].id != item.id &&
           item.weekday == processed[i].weekday &&
           processed[i].period < item.period + item.size &&
           processed[i].period + processed[i].size > item.period,
       );
 
       mutualOverlap.forEach((item) => {
-        if (item._offset != -1) offsetList.push(item._offset);
+        if (item._offset !== -1) offsetList.push(item._offset);
       });
 
       let j = 0;
@@ -117,8 +118,7 @@
       processed[i]._offset = j;
 
       for (let k = 0; k < processed.length; k++) {
-        if (processed[k]._overlap || processed[i].weekday != processed[k].weekday || i == k)
-          continue;
+        if (processed[i].weekday != processed[k].weekday || i == k) continue;
 
         if (processed[i].section.subject.id == processed[k].section.subject.id) {
           processed[k]._overlap = true;
