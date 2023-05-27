b9e602b21cb9bc72fa27cd3223133bbd8579573b
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2023-05-27 11:04:19 +0700

    refactor(client): use built in func

--- client/src/routes/course/CourseDetail.svelte
+++ client/src/routes/course/CourseDetail.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
   export let courseData: API.Course;
 
-  courseData.detail.sort((a, b) => (a.type > b.type ? 1 : b.type > a.type ? -1 : 0));
-  courseData.detail.sort((a, b) =>
-    a.subject.code > b.subject.code ? 1 : b.subject.code > a.subject.code ? -1 : 0,
-  );
+  courseData.detail.sort((a, b) => a.subject.code.localeCompare(b.subject.code) || a.type.localeCompare(b.type));
 </script>
 
-<form>
+<div>
   <div>
     <h1 class="mb-4 block text-center text-2xl font-bold">{courseData.name}</h1>
   </div>
-  <div class= "grid grid-cols-3">
+  <div class="grid grid-cols-3">
     {#each courseData.detail as course}
       <p class=" col-span-2 text-left">{course.subject.code} {course.subject.name}</p>
       <p class="text-right capitalize">{course.type}</p>
     {/each}
   </div>
-</form>
+</div>
