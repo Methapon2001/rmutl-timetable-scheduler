0a390ae26c201a48db483c638f0619b12c531265
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2023-06-01 19:33:30 +0700

    fix(client/guest-view): publish only data

--- client/src/routes/+page.ts
+++ client/src/routes/+page.ts
@@ -2,11 +2,12 @@ import type { PageLoad } from './$types';
 import { PUBLIC_API_HOST } from '$env/static/public';
 
 export const load = (async ({ fetch, depends }) => {
-
   depends('data:scheduler');
 
   const schedulerData = async () => {
-    return await fetch(`${PUBLIC_API_HOST}/api/scheduler?limit=9999&publish=true`).then((res) => res.json());
+    return await fetch(`${PUBLIC_API_HOST}/api/scheduler?limit=9999&publish=true`).then((res) =>
+      res.json(),
+    );
   };
 
   return {
