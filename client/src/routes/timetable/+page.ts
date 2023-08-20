b6b144f123a2043204a35e6c6e266f2380c3d097
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2023-08-20 13:32:56 +0000

    feat: switch semester / year in timetable

--- client/src/routes/timetable/+page.ts
+++ client/src/routes/timetable/+page.ts
@@ -1,26 +1,48 @@
-import type { PageLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
-import { PUBLIC_API_HOST } from '$env/static/public';
+import type { PageLoad } from "./$types";
+import { redirect } from "@sveltejs/kit";
+import { PUBLIC_API_HOST } from "$env/static/public";
+import { info } from "$lib/stores";
+
+let currentInfo: API.Info | undefined = undefined;
+info.subscribe((v) => (currentInfo = v));
 
 export const load = (async ({ fetch, parent, depends }) => {
   const { session } = await parent();
 
-  depends('data:scheduler');
+  depends("data:scheduler");
 
-  if (!session) throw redirect(302, '/login?redirect=/timetable');
+  if (!session) throw redirect(302, "/login?redirect=/timetable");
 
   const schedulerData = async () => {
-    return await fetch(`${PUBLIC_API_HOST}/api/scheduler?limit=9999`).then((res) => res.json());
+    return await fetch(
+      `${PUBLIC_API_HOST}/api/scheduler?limit=9999&${
+        currentInfo
+          ? `&year=${currentInfo.year}&semester=${currentInfo.semester}`
+          : ""
+      }`,
+    ).then((res) => res.json());
   };
 
   const sectionData = async () => {
     return await fetch(
-      `${PUBLIC_API_HOST}/api/section?limit=9999&createdByUserId=${session.user.id}`,
+      `${PUBLIC_API_HOST}/api/section?limit=9999&createdByUserId=${
+        session.user.id
+      }${
+        currentInfo
+          ? `&year=${currentInfo.year}&semester=${currentInfo.semester}`
+          : ""
+      }`,
     ).then((res) => res.json());
   };
 
   const requestGroup = async () => {
-    const res = await fetch(`${PUBLIC_API_HOST}/api/group?limit=9999`);
+    const res = await fetch(
+      `${PUBLIC_API_HOST}/api/group?limit=9999${
+        currentInfo
+          ? `&year=${currentInfo.year}&semester=${currentInfo.semester}`
+          : ""
+      }`,
+    );
     const body = await res.json();
     return body as {
       data: API.Group[];
