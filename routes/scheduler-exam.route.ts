4c328fd5a2d4f43b88a89ee93d182fdd70895a4e
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2023-10-27 20:35:43 +0700

    refactor: update websocket communition

--- routes/scheduler-exam.route.ts
+++ routes/scheduler-exam.route.ts
@@ -8,6 +8,8 @@ import {
 } from "../services/scheduler-exam.service";
 import schedulerExamSchema from "./validation/scheduler-exam.schema";
 
+const UPDATE_SIGNAL = { update: 2 };
+
 export default async (server: FastifyInstance) => {
   function broadcast(message: string) {
     server.websocketServer.clients.forEach((client) => {
@@ -18,7 +20,7 @@ export default async (server: FastifyInstance) => {
   server.post("/api/scheduler-exam", {
     onRequest: auth(),
     onResponse: (_request, _reply) => {
-      broadcast("Schedule exam updated.");
+      broadcast(JSON.stringify(UPDATE_SIGNAL));
     },
     handler: createSchedulerExam,
     schema: {
@@ -46,7 +48,7 @@ export default async (server: FastifyInstance) => {
   server.put("/api/scheduler-exam/:id", {
     onRequest: auth(),
     onResponse: (_request, _reply) => {
-      broadcast("Schedule exam updated.");
+      broadcast(JSON.stringify(UPDATE_SIGNAL));
     },
     handler: updateSchedulerExam,
     schema: {
@@ -58,7 +60,7 @@ export default async (server: FastifyInstance) => {
   server.delete("/api/scheduler-exam/:id", {
     onRequest: auth(),
     onResponse: (_request, _reply) => {
-      broadcast("Schedule exam updated.");
+      broadcast(JSON.stringify(UPDATE_SIGNAL));
     },
     handler: deleteSchedulerExam,
     schema: {
--- routes/websocket.route.ts
+++ routes/websocket.route.ts
@@ -3,5 +3,15 @@ import { FastifyInstance } from "fastify";
 export default async (server: FastifyInstance) => {
   server.get("/websocket", { websocket: true }, (connection, _) => {
     connection.setEncoding("utf-8");
+    connection.socket.on("message", (msg) => {
+      const obj = JSON.parse(msg.toString());
+
+      if (typeof obj === "object" && (obj as Object).hasOwnProperty("update"))
+        server.websocketServer.clients.forEach((client) => {
+          if (client.readyState === 1) {
+            client.send(JSON.stringify({ update: obj.update }));
+          }
+        });
+    });
   });
 };
